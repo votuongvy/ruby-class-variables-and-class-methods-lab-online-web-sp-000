d4a3c1af73e1e51515ac468f8166e259bebfc50a
vy vo <tuonggvy.vo@gmail.com>
2020-12-02 04:47:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -114,4 +114,10 @@ class Song
     return genre_count
   end
   
+  def self.artist_count
+    artist_count = Hash.new(0)
+    @@artists.each { |artist| artist_count[artist] += 1}
+    return artist_count
+  end
+  
 end

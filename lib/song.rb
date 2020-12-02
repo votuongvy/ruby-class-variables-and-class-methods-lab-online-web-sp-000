b11e6078f646e7f54450ac1325643c26f89386f9
vy vo <tuonggvy.vo@gmail.com>
2020-12-02 04:30:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -80,12 +80,14 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genres << genre
   end
 
   def self.count
